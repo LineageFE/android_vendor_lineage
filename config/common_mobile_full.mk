69976f7446a1a761ba59d93ff41cfbf9553939e2
Dyneteve <dyneteve@hentaios.com>
2023-06-13 19:32:34 +0000

    overlay: Allow new SystemUI clocks
    Change-Id: I51ab1eb06b1e77bd8048d5064f811e8e69884b24
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config/common_mobile_full.mk
+++ config/common_mobile_full.mk
@@ -50,6 +50,17 @@ PRODUCT_PACKAGES += \
     FontHarmonySansOverlay \
     FontOppoSansOverlay
 
+# Clocks
+PRODUCT_PACKAGES += \
+    SystemUIClocks-BigNum \
+    SystemUIClocks-Calligraphy \
+    SystemUIClocks-Flex \
+    SystemUIClocks-Growth \
+    SystemUIClocks-Inflate \
+    SystemUIClocks-Metro \
+    SystemUIClocks-NumOverlap \
+    SystemUIClocks-Weather
+
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/dictionaries
